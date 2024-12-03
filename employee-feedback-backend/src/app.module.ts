4ccbcf8c96713904072216b6b6859fa4bef0ece8
Sam Flavin <seamus@Sams-Laptop.local>
2024-12-03 21:28:59 +0800

    setting up atlas render env variables

--- employee-feedback-backend/src/app.module.ts
+++ employee-feedback-backend/src/app.module.ts
@@ -8,7 +8,7 @@ import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/employee-review-system'), // Correct URI for local MongoDB
+    MongooseModule.forRoot(process.env.MONGO_URI),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
